1e393eecc45659722767086fd900b227cb3cd397
Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>
2019-12-07 01:24:49 +0200

    Update Arduino_Code.ino

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,16 +1,29 @@
+
 /*
 
-Arduino Full Home Automation Project
+  Arduino Full Home Automation Project
 
-Created by Tudor Cernat
- 
- */
+  Created by Tudor Cernat
+
+*/
 
 
+#include <SPI.h>
+#include <Ethernet.h>
 
+// Network mask, IP and port
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 0, 250);
+EthernetServer server(80);
 
 #include<LiquidCrystal.h>
 LiquidCrystal lcd(2, 3, 5, 6, 7, 8);
+
+#include <EduIntro.h>
+DHT11 dht11(9);
+
 
 
 int releu1 = 49; // Pini pentru relee
@@ -30,11 +43,18 @@ int releu14 = 36;
 int releu15 = 35;
 int releu16 = 34;
 
+int temperatura, umid;
+String readString;
+String r1;
+
 
 
 
 void setup() //                        --- VOID SETUP ---
 {
+  //pinMode(4, OUTPUT); // SD Card kill switch
+  //digitalWrite(4, HIGH);
+
   pinMode(releu1, OUTPUT); // Definirea pinilor digitali
   pinMode(releu2, OUTPUT);
   pinMode(releu3, OUTPUT);
@@ -75,15 +95,20 @@ void setup() //                        --- VOID SETUP ---
   digitalWrite(LED_BUILTIN, HIGH); // Pornire LED Galben
 
   lcd.begin(20, 4);
-
   lcd.clear();
 
-  lcd.print("Setup");
+  Ethernet.begin(mac, ip);
+  server.begin();
+  lcd.print("IP: ");
+  lcd.print(Ethernet.localIP());
+  lcd.setCursor(0, 1);
+  lcd.print("Initializare");
+
   delay(2000); // 2 secunde
 
   lcd.clear();
-    
- 
+
+
   digitalWrite(LED_BUILTIN, LOW);
 }
 
@@ -94,11 +119,111 @@ void setup() //                        --- VOID SETUP ---
 
 void loop()
 {
-  lcd.clear();
   digitalWrite(LED_BUILTIN, HIGH);
-  lcd.print("Loop");
-  delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.print("IP: ");
+  lcd.print(Ethernet.localIP());
+
+  dht11.update();
+
+  temperatura = dht11.readCelsius() - 2;
+  umid = dht11.readHumidity();
+
+  listenForEthernetClients();
+
+  lcd.setCursor(0, 2);
+  lcd.print("T ");
+  lcd.print(temperatura);
+  lcd.print("'C  - U ");
+  lcd.print(umid);
+  lcd.print("%");
+
+  if (readString.indexOf("on1") > 0) //checks for on
+  {
+    digitalWrite(releu1, LOW);  // 1
+    lcd.setCursor(0, 3);
+    r1 = "Releu 1 pornit";
+    lcd.print(r1);
+  }
+  if (readString.indexOf("off") > 0) //checks for off
+  {
+    digitalWrite(releu1, HIGH);  // 1
+    lcd.setCursor(0, 3);
+    r1 = "Releu 1 oprit";
+    lcd.print(r1);
+  }
+  //clearing string for next read
+  readString = "";
+
+  delay(500);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
- 
+  delay(500);
+
 } // final void loop
+
+void listenForEthernetClients()
+{
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client)
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("Client conectat");
+    // an http request ends with a blank line
+    bool currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+
+        readString += c;
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println();
+          client.println("<HTML>");
+          client.print("<HEAD>");
+          client.print("<meta http-equiv=\"refresh\" content=\"5\">");
+          client.print("<TITLE />Resedinta Cernat</title>");
+          client.print("</head>");
+
+          client.println("<BODY>");
+          client.print("Temperatura: ");
+          client.print(temperatura);
+          client.print(" 'C");
+          client.println("<br />");
+          client.print("Umiditate: " + String(umid));
+          client.print("%");
+          client.println("<br />");
+
+          client.print("Releu 1: ");
+          client.print("<a href='/?on1' target='inlineframe'><button type='button'>ON</button></a>");
+          client.print("<a href='/?off' target='inlineframe'><button type='button'>OFF</button></a>");
+          client.print("   ");
+          client.print(r1);
+
+          client.println("<IFRAME name=inlineframe style='display:none'>");
+          client.println("</IFRAME>");
+
+          client.println("</BODY>");
+          client.println("</HTML>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+  }
+}
